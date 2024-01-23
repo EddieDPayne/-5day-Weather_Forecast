f65ddb44623a8a2f9194fb40ec9fda5409e2a12c
Eddie Payne <eddie_p89@hotmail.com>
2024-01-23 16:52:13 +1100

    Removed commented out code from html and edited the display forecast function so that it displays the forecast correctly

--- assets/script.js
+++ assets/script.js
@@ -6,21 +6,21 @@ function displayTime() {
 setInterval(displayTime, 1000);
 
 /////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-
+// Global Variables
 var searchBtn = $('.search-btn');
 var searchForm = document.getElementById('search-form');
 var searchInput = document.getElementById('city-search');
 var weatherContainer = $('#display-weather');
 
+var apiKey = 'f274417977bfe3e7532f91875384063b';
+var lon;
+var lat;
 
 
 /////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 
-var apiKey = 'f274417977bfe3e7532f91875384063b';
-var lon;
-var lat;
-
+// Find city API & Fetch request
 function cityApi(cityName) {
   
   
@@ -38,7 +38,7 @@ function cityApi(cityName) {
       lat = data[0].lat;
       lon = data[0].lon;
       weatherApi(lat, lon)
-      // displayWeather(data);
+      
       
     })
     .catch(function (error) {
@@ -62,16 +62,22 @@ function weatherApi(lat, lon) {
     .then(function (data) {
       console.log(data)
      for (let i = 0; i < data.list.length; i+=8) {
+      // gets revelant day
+      var timestamp = data.list[i].dt * 1000;
+      // uses dayjs to display the relevant day
+      var weekDay = dayjs(timestamp).format('dddd');
     var htmlEl = `
     <div class="card" style="width: 12rem;">
     <img src= "https://openweathermap.org/img/w/${data.list[i].weather[0].icon}.png" style="width: 40%">
     <div class="card-body">
-      <h5 class="card-title">Card title</h5>
+      <h5 class="card-title">${weekDay}</h5>
       <p class="card-text">Temp: ${data.list[i].main.temp}</p>
-      <a href="#" class="btn btn-primary">Go somewhere</a>
+      <p class="card-text">Humidty: ${data.list[i].main.humidity}</p>
+      <p class="card-text">Wind: ${data.list[i].wind.speed}</p>
+      </div> 
     </div> 
-  </div> 
-    `
+      `
+     
     var htmlDiv = $("<div>")
     htmlDiv.html(htmlEl);
       $("#display-forecast-list").append(htmlDiv);
